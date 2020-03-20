517faf153924118bb75aa546c36ebc80fa9b77bd
Alejandro Masin <masinalejandro.crosetto@gmail.com>
2020-03-20 17:01:00 -0300

    subiendo npm

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # saludo-random
-Envía saludos random
+    Envía saludos random en 21 idiomas diferentes.
+
+## Installation
+    npm install @alemas/saludo-random --save
+
+## Usage
+    const randomSaludo = require('@alemas/saludo-random');
+
+    const saludo = randomSaludo();
--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-console.log('algo');
+const _ = require('lodash');
 
+const saludos = [
+"Salut! desde Francia",
+"Hallo! desde Alemania",
+"Ciao! desde Italia",
+"Ahoj! desde Republica Checa",
+"YAH sahs! desde Grecia",
+"Bog! desde Croacia",
+"Hallo! desde Holanda",
+"Szia! desde Hungria",
+"Hej! desde Suecia",
+"Czesc! desde Polonia",
+"¡Hola! desde Mexico",
+"Ní háo desde China",
+"Kon'nichiwa desde Japon",
+"Annyeonghaseyo desde Korea",
+"Shalom! desde Israel",
+"Sàwàddee ká, sàwàddee kráb! desde Tailandia",
+"Merhaba! desde Turquia",
+"Zdrahvdz-vuee-tyeh! desde Rusia",
+"Marhaba! desde Egipto",
+"Hujambo! desde Kenya",
+"Olá! desde Brasil"
+];
+
+function randomSaludo() {
+    return _.sample(saludos);
+}
+  
+module.exports = {
+    randomSaludo,
+};
